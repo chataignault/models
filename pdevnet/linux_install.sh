5142d98108e443e1339c87155d08d2a971a92971
chataignault <leonchat@orange.fr>
2024-12-18 23:24:09 +0000

    start defining image

---
+++ pdevnet/linux_install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+sudo apt update -y
+sudo apt install build-essential
+sudo apt install python3-dev
+sudo apt install \
+    libssl-dev \
+    zlib1g-dev \
+    libbz2-dev \
+    libffi-dev \
+    liblzma-dev \
+    libsqlite3-dev \
+    libreadline-dev \
+    libncurses5-dev
+sudo apt install pkg-config
+
+
