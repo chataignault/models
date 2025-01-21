8b08c288c6433146d8660929abf924471d6be37a
chataignault <leonchat@orange.fr>
2025-01-21 18:44:39 +0000

    comly paper architecture

--- unet/utils_torch/classes.py
+++ unet/utils_torch/classes.py
@@ -30,38 +30,21 @@ class SinusoidalPositionEmbeddings(nn.Module):
         return pe
 
 
-class Block(nn.Module):
+class ResBlock(nn.Module):
     def __init__(
         self,
         in_ch: int,
         out_ch: int,
-        time_emb_dim: int,
-        up: bool = False,
-        attention: bool = False,
     ):
         """
         in_ch refers to the number of channels in the input to the operation and out_ch how many should be in the output
         """
         super().__init__()
 
-        # self.time_mlp = nn.Linear(
-        #     time_emb_dim,
-        #     1,
-        #     # time_emb_dim
-        # )
-
-        if up:
-            self.conv1 = nn.Conv2d(2 * in_ch, out_ch, 3, padding=1)
-            self.transform = nn.ConvTranspose2d(
-                out_ch, out_ch, kernel_size=4, stride=2, padding=1
-            )
-
-        else:
-            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1)
-            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
-
-        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1)
-        self.bnorm1 = nn.BatchNorm2d(out_ch)
+        self.lintemb = nn.Linear(in_ch, out_ch)
+        self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1, stride=1)
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1, stride=1)
+        self.bnorm1 = nn.BatchNorm2d(in_ch)
         self.bnorm2 = nn.BatchNorm2d(out_ch)
         self.relu = nn.ReLU()
         self.dropout = nn.Dropout(0.05)
@@ -74,23 +57,72 @@ class Block(nn.Module):
         The time embedding should get added the output from the input convolution
         A second convolution should be applied and finally passed through the self.transform.
         """
-        # t = self.time_mlp(t)
-        # print(x.shape)
-        # print(t.shape)
-        x = x + t  # unsqueeze(1).unsqueeze(1)
-        x = self.conv1(x)
-        x = self.relu(x)
-        # .repeat(
-        #     (1, x.size(1), x.size(-1), x.size(-2) // t.shape[1])
-        # )
+        h = x
         x = self.bnorm1(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+        t = self.lintemb(self.relu(t))
+        x = x + t
+        x = self.bnorm2(x)
         x = self.dropout(x)
         x = self.conv2(x)
         x = self.relu(x)
-        x = self.transform(x)
-        x = self.bnorm2(x)
+        x = x + h
 
         return x
+
+
+class Block(nn.Module):
+    def __init__(
+        self,
+        in_ch: int,
+        out_ch: int,
+        up: bool = False,
+    ):
+        """
+        in_ch refers to the number of channels in the input to the operation and out_ch how many should be in the output
+        """
+        super().__init__()
+
+        self.lintemb = nn.Linear(in_ch, out_ch)
+
+        if up:
+            self.conv1 = nn.Conv2d(2 * in_ch, out_ch, 3, padding=1)
+            self.transform = nn.ConvTranspose2d(
+                out_ch, out_ch, kernel_size=4, stride=2, padding=1
+            )
+
+        else:
+            self.conv1 = nn.Conv2d(in_ch, out_ch, 3, padding=1, stride=1)
+            self.transform = nn.Conv2d(out_ch, out_ch, 4, 2, 1)
+
+        self.conv2 = nn.Conv2d(out_ch, out_ch, 3, padding=1, stride=1)
+        self.bnorm1 = nn.BatchNorm2d(in_ch)
+        self.bnorm2 = nn.BatchNorm2d(out_ch)
+        self.relu = nn.ReLU()
+        self.dropout = nn.Dropout(0.05)
+
+    def forward(self, x: Tensor, t: Tensor) -> Tensor:
+        """
+        Define the forward pass making use of the components above.
+        Time t should get mapped through the time_mlp layer + a relu
+        The input x should get mapped through a convolutional layer with relu / batchnorm
+        The time embedding should get added the output from the input convolution
+        A second convolution should be applied and finally passed through the self.transform.
+        """
+        h = x
+        x = self.bnorm1(x)
+        x = self.relu(x)
+        x = self.conv1(x)
+        t = self.lintemb(self.relu(t))
+        x = x + t
+        x = self.bnorm2(x)
+        x = self.dropout(x)
+        x = self.conv2(x)
+        x = self.relu(x)
+        x = x + h
+
+        return self.transform(x), x
 
 
 class SimpleUnet(nn.Module):
@@ -98,27 +130,26 @@ class SimpleUnet(nn.Module):
     A simplified variant of the Unet architecture.
     """
 
-    def __init__(self):
+    def __init__(
+            self,
+            time_emb_dim:int=4,
+            ):
         super().__init__()
         image_channels = 1
         down_channels = [
-            32,
-            64,
+            4,
+            16,
             128,
         ]
-        up_channels = [
-            128,
-            64,
-            32,
-        ]
-        out_dim = 1
-        time_emb_dim = 7
+        up_channels = down_channels[::-1]
+        
+        self.time_emb_dim = time_emb_dim
 
-        # self.pos_emb = nn.Sequential(
-        self.pos_emb = (
-            SinusoidalPositionEmbeddings(dim=time_emb_dim)
-            # nn.Linear(time_emb_dim, time_emb_dim),
-            # nn.ReLU(),
+        self.pos_emb = nn.Sequential(
+            SinusoidalPositionEmbeddings(dim=time_emb_dim),
+            nn.Linear(time_emb_dim, 4 * time_emb_dim),
+            nn.ReLU(),
+            nn.Linear(4 * time_emb_dim, 4 * time_emb_dim)
         )
 
         self.init_conv = nn.Conv2d(
@@ -126,6 +157,7 @@ class SimpleUnet(nn.Module):
             out_channels=down_channels[0],
             kernel_size=3,
             padding=1,
+            stride=1,
         )
 
         self.downsampling = nn.Sequential(
@@ -133,73 +165,50 @@ class SimpleUnet(nn.Module):
                 Block(
                     down_channels[i],
                     down_channels[i + 1],
-                    time_emb_dim,
-                    # attention=(i == 1),
                 )
                 for i in range(len(down_channels) - 1)
             ]
         )
-        self.conv_int1 = nn.Conv2d(128, 64, 1)
-        self.relu = nn.ReLU()
-        self.conv_int2 = nn.Conv2d(64, 128, 1)
 
+        self.resint1 = ResBlock(down_channels[-1], 64)
         self.bnorm = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU()
+        self.resint2 = ResBlock(64, up_channels[0])
 
         self.upsampling = nn.Sequential(
             *[
                 Block(
                     up_channels[i],
                     up_channels[i + 1],
-                    time_emb_dim,
                     up=True,
-                    # attention=(i == 0),
                 )
                 for i in range(len(up_channels) - 1)
             ]
         )
 
+        self.bnorm_out = nn.BatchNorm2d(up_channels[-1])
         self.out_conv = nn.Conv2d(
-            in_channels=up_channels[-1], out_channels=out_dim, kernel_size=1
+            in_channels=up_channels[-1], out_channels=1, kernel_size=1
         )
 
     def forward(self, x: Tensor, t: Tensor):
-        # print(x.shape)
-        t_ = self.pos_emb(t, dim=x.size(-1) * x.size(-2))
-        # print(t_.shape)
-        # print((t_.unsqueeze(1).reshape((x.size(0), 1, x.size(2), x.size(3)))).shape)
-        x = x + t_.unsqueeze(1).reshape((x.size(0), 1, x.size(2), x.size(3)))
-        # t.unsqueeze(1).unsqueeze(1).repeat(
-        #     (1, x.size(1), x.size(-1), x.size(-2) // t.shape[1])
-        # )
-
+        t = self.pos_emb(t, dim=self.time_emb_dim)
         x = self.init_conv(x)
-        x = self.relu(x)
-
-        x_down_ = []
+        x_down_ = [x]
         for block in self.downsampling.children():
-            t_ = self.pos_emb(t, dim=(x.size(-1) * x.size(-2)))
-            t_ = t_.unsqueeze(1).reshape((x.size(0), 1, x.size(-1), x.size(-2)))
-            x = block(x, t_)
-            x_down_.append(x)
-
-        x = self.conv_int1(x)
-        x = self.relu(x)
+            x, h = block(x, t)
+            x_down_.append(h)
+        x = self.resint1(x)
         x = self.bnorm(x)
-
-        x = self.conv_int2(x)
         x = self.relu(x)
-
+        x = self.resint2(x)
         for k, block in enumerate(self.upsampling.children(), 1):
             residual = x_down_[-k]
             # crop residual to match x dimensions
-            residual = CenterCrop(x.size(2))(residual)
+            # residual = CenterCrop(x.size(2))(residual)
             x_extended = torch.cat((x, residual), dim=1)
-            t_ = self.pos_emb(t, dim=(x_extended.size(-1) * x_extended.size(-2)))
-            t_ = t_.unsqueeze(1).reshape(
-                (x_extended.size(0), 1, x_extended.size(-1), x_extended.size(-2))
-            )
-            x = block(x_extended, t_)
-
+            x = block(x_extended, t)
+        x = self.bnorm_out(x)
+        x = self.relu(x)
         x = self.out_conv(x)
-
         return x
