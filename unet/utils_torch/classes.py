e88e163437c50ec078048f3627787593666ce547
chataignault <leonchat@orange.fr>
2025-01-31 17:56:31 +0000

    add attention projection

--- unet/utils_torch/classes.py
+++ unet/utils_torch/classes.py
@@ -132,15 +132,23 @@ class Block(nn.Module):
 class AttentionBlock(nn.Module):
     """ """
 
-    def __init__(self, in_ch: int, hidden_dim: int, time_embed_dim: int):
+    def __init__(
+        self,
+        in_ch: int,
+        in_dim: int,
+        hidden_dim: int,
+        n_heads: int,
+        time_embed_dim: int,
+    ):
         super().__init__()
         self.lintemb = nn.Linear(time_embed_dim, in_ch)
         self.relu = nn.ReLU()
         self.bnorm = nn.BatchNorm2d(in_ch)
-        self.k = nn.Linear(49, 49)
-        self.q = nn.Linear(49, 49)
-        self.v = nn.Linear(49, 49)
-        self.attention = nn.MultiheadAttention(49, 7, batch_first=True)
+        self.k = nn.Linear(in_dim, hidden_dim, biais=False)
+        self.q = nn.Linear(in_dim, hidden_dim, biais=False)
+        self.v = nn.Linear(in_dim, hidden_dim, biais=False)
+        self.attention = nn.MultiheadAttention(hidden_dim, n_heads, batch_first=True)
+        self.proj = nn.Linear(hidden_dim, in_ch)
 
     def forward(self, x, t):
         t = self.relu(self.lintemb(t)).unsqueeze(-1).unsqueeze(-1)
@@ -153,7 +161,45 @@ class AttentionBlock(nn.Module):
         key = self.k(x)
         value = self.v(x)
         x, _ = self.attention(query, key, value)
-        # x, _ = self.attention(x, x, x)
+        x = self.proj(x)
+        x = x.reshape((N, B, D, D))
+        return x
+
+
+class AttentionBlock2(nn.Module):
+    """ """
+
+    def __init__(
+        self,
+        in_ch: int,
+        in_dim: int,
+        hidden_dim: int,
+        time_embed_dim: int,
+        nheads: int = 4,
+    ):
+        super().__init__()
+        self.lintemb = nn.Linear(time_embed_dim, in_ch)
+        self.relu = nn.ReLU()
+        self.bnorm = nn.BatchNorm2d(in_ch)
+        self.k = nn.Linear(in_dim**2, hidden_dim)
+        self.q = nn.Linear(in_dim**2, hidden_dim)
+        self.v = nn.Linear(in_dim**2, hidden_dim)
+        self.attention = nn.MultiheadAttention(hidden_dim, nheads, batch_first=True)
+        self.proj = nn.Linear(hidden_dim, in_dim**2)
+
+    def forward(self, x, t):
+        t = self.relu(self.lintemb(t)).unsqueeze(-1).unsqueeze(-1)
+        x = x + t
+        x = self.bnorm(x)
+        x = self.relu(x)
+        N, B, D, _ = x.shape
+        x = x.reshape((N, B, D * D)).transpose(1, 2)
+        query = self.q(x)
+        key = self.k(x)
+        value = self.v(x)
+        x, _ = self.attention(query, key, value)
+        x = self.proj(x)
+        x = x.transpose(1, 2).view(N, D, D, D)
         x = x.reshape((N, B, D, D))
         return x
 
@@ -338,5 +384,97 @@ class Unet(nn.Module):
         x = x + x_down_[0]
         x = self.bnorm_out(x)
         x = self.relu(x)
+        x = self.out_conv(x)
+        return x
+
+
+class Unet2(nn.Module):
+    """
+    A simplified variant of the Unet architecture.
+    """
+
+    def __init__(
+        self,
+        time_emb_dim: int = 4,
+    ):
+        super().__init__()
+        image_channels = 1
+        down_channels = [
+            8,
+            32,
+            128,
+        ]
+        up_channels = down_channels[::-1]
+
+        self.time_emb_dim = time_emb_dim
+
+        self.pos_emb = nn.Sequential(
+            SinusoidalPositionEmbeddings(dim=time_emb_dim),
+            nn.Linear(time_emb_dim, 4 * time_emb_dim),
+            nn.ReLU(),
+            nn.Linear(4 * time_emb_dim, 4 * time_emb_dim),
+        )
+
+        self.init_conv = nn.Conv2d(
+            in_channels=image_channels,
+            out_channels=down_channels[0],
+            kernel_size=3,
+            padding=1,
+            stride=1,
+        )
+
+        self.downsampling = nn.Sequential(
+            *[
+                Block(down_channels[i], down_channels[i + 1], 4 * time_emb_dim)
+                for i in range(len(down_channels) - 1)
+            ]
+        )
+
+        self.resint1 = ResBlock(down_channels[-1], 4 * time_emb_dim)
+        self.bnorm = nn.BatchNorm2d(down_channels[-1])
+        self.attention_int = AttentionBlock2(down_channels[-1], 7, 24, 4 * time_emb_dim)
+        self.relu = nn.ReLU()
+        self.silu = nn.SiLU()
+
+        self.resint2 = ResBlock(down_channels[-1], 4 * time_emb_dim)
+
+        self.upsampling = nn.Sequential(
+            *[
+                Block(
+                    up_channels[i],
+                    up_channels[i + 1],
+                    4 * time_emb_dim,
+                    up=True,
+                )
+                for i in range(len(up_channels) - 1)
+            ]
+        )
+
+        self.bnorm_out = nn.BatchNorm2d(up_channels[-1])
+        self.out_conv = nn.Conv2d(
+            in_channels=up_channels[-1], out_channels=1, kernel_size=1
+        )
+
+    def forward(self, x: Tensor, t: Tensor):
+        t = self.pos_emb(t)
+        x = self.init_conv(x)
+        x_down_ = [x]
+        for block in self.downsampling.children():
+            x, h = block(x, t)
+            x_down_.append(h)
+        x_down_.append(x)
+        x = self.resint1(x, t)
+        x = self.attention_int(x, t)
+        x = self.bnorm(x)
+        x = self.silu(x)
+        x = self.resint2(x, t)
+        for k, block in enumerate(self.upsampling.children(), 1):
+            residual = x_down_[-k]
+            x_extended = torch.cat([x, residual], dim=1)
+            x, _ = block(x_extended, t)
+        # add the ultimate residual from the initial convolution
+        x = x + x_down_[0]
+        x = self.bnorm_out(x)
+        x = self.silu(x)
         x = self.out_conv(x)
         return x
