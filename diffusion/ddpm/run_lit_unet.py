correct variance parameters

diffusion/ddpm/run_lit_unet.py
@@ -5,6 +5,7 @@ import numpy as np
 import datetime as dt
 from rich import print
 from torch.utils.tensorboard import SummaryWriter
+import torch.nn.functional as F
 import lightning as L
 from lightning.pytorch.callbacks import LearningRateMonitor, DeviceStatsMonitor
 from lightning.pytorch.loggers import TensorBoardLogger
@@ -15,7 +16,12 @@ from matplotlib.pyplot import imshow
 from utils.logger import get_logger
 from utils_torch.unet import LitUnet, SimpleUnet, Unet
 from utils.fashion_mnist_dataloader import get_dataloader
-from utils_torch.diffusion import sample, linear_beta_schedule, cosine_beta_schedule
+from utils_torch.diffusion import (
+    sample,
+    linear_beta_schedule,
+    cosine_beta_schedule,
+    sigmoid_beta_schedule,
+)
 
 DEFAULT_IMG_SIZE = 28
 
@@ -114,11 +120,11 @@ if __name__ == "__main__":
     torch.backends.cuda.matmul.allow_tf32 = True
     torch.backends.cudnn.allow_tf32 = True
 
-    betas = linear_beta_schedule(timesteps=T, device=device)
+    # betas = linear_beta_schedule(timesteps=T, device=device)
+    betas = sigmoid_beta_schedule(timesteps=T, device=device)
     # betas = cosine_beta_schedule(timesteps=T, device=device)
     alphas = 1.0 - betas
     alphas_cumprod = torch.cumprod(alphas, -1)
-    import torch.nn.functional as F
 
     alphas_cumprod_prev = F.pad(alphas_cumprod[:-1], (1, 0), value=1.0)
 
@@ -163,13 +169,15 @@ if __name__ == "__main__":
     # train
     unet = LitUnet(
         unet=unet,
+        betas=betas,
+        sqrt_recip_alphas=sqrt_recip_alphas,
         sqrt_alphas_cumprod=sqrt_alphas_cumprod,
         sqrt_one_minus_alphas_cumprod=sqrt_one_minus_alphas_cumprod,
+        posterior_variance=posterior_variance,
         T=T,
         device=device,
         lr=lr,
         writer=writer,
-        posterior_variance=posterior_variance,
         img_size=IMG_SIZE,
     )
     trainer = L.Trainer(

diffusion/ddpm/utils_torch/diffusion.py
@@ -42,21 +42,47 @@ def cosine_beta_schedule(
     return end * (1.0 - (torch.cos(np.pi * 0.5 * (steps / timesteps))) ** 2)
 
 
+def sigmoid_beta_schedule(timesteps, device, start=-3, end=3, tau=1, clamp_min=1e-5):
+    """
+    sigmoid schedule
+    proposed in https://arxiv.org/abs/2212.11972 - Figure 8
+    better for images > 64x64, when used during training
+    """
+    steps = timesteps + 1
+    t = (
+        torch.linspace(
+            0,
+            timesteps,
+            steps,
+            # dtype = torch.float64
+        )
+        / timesteps
+    )
+    v_start = torch.tensor(start / tau).sigmoid()
+    v_end = torch.tensor(end / tau).sigmoid()
+    alphas_cumprod = (-((t * (end - start) + start) / tau).sigmoid() + v_end) / (
+        v_end - v_start
+    )
+    alphas_cumprod = alphas_cumprod / alphas_cumprod[0]
+    betas = 1 - (alphas_cumprod[1:] / alphas_cumprod[:-1])
+    return torch.clip(betas, 0, 0.999).to(device)
+
+
 def forward_diffusion_sample(
     x_0: Tensor,
     t: Tensor,
     sqrt_alphas_cumprod: Tensor,
     sqrt_one_minus_alphas_cumprod: Tensor,
+    noise: Tensor,
     device: str = "cpu",
 ) -> Tensor:
     """
     Takes an image and a timestep as input and
-    returns the noisy version of it
-    Hint: use the get_index_from_list function to select the right values at time t.
+    returns the noisy version of it at a given timestep, using the Gaussian noise reparameterisation
     """
     mean = (get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape) * x_0).to(device)
     std = get_index_from_list(sqrt_one_minus_alphas_cumprod, t, x_0.shape).to(device)
-    return mean + std * torch.randn_like(x_0).to(device)
+    return mean + std * noise
 
 
 @torch.no_grad()
@@ -71,14 +97,13 @@ def sample_timestep(
     device: str,
 ) -> Tensor:
     """
-    Calls the model to predict the noise in the image and returns
-    the denoised image.
+    Calls the model to predict the noise in the image and returns the denoised image.
     Applies noise to this image, if we are not in the last step yet.
-    Note that it also needs additional arguments about the posterior_variance, sqrt_minus_alphas_cumprod and sqrt_recip_alphas.
     """
     x = sqrt_recip_alphas[i] * (
         x - (posterior_variance[i]) / sqrt_one_minus_alphas_cumprod[i] * (model(x, t))
     )
+    # x = x.clamp(-1. 1.)
     if i > 0:
         z = torch.randn_like(x).to(device)
         x += torch.sqrt(posterior_variance[i]) * z
@@ -102,7 +127,7 @@ def sample(
     img = torch.randn(
         shape,
         device=device,
-    )  # ! check whether the variance corresponds to forward pass
+    )
     imgs = []
     for i in tqdm(
         reversed(range(1, T)), desc="sampling loop time step", total=T

diffusion/ddpm/utils_torch/training.py
@@ -1,3 +1,4 @@
+import torch
 from torch import Tensor
 import torch.nn.functional as F
 
@@ -16,10 +17,8 @@ def get_loss(
     """
     Define the right loss given the model, the true x_0 and the time t
     """
+    noise = torch.randn_like(x_0).to(device)
     x_t = forward_diffusion_sample(
-        x_0, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, device=device
+        x_0, t, sqrt_alphas_cumprod, sqrt_one_minus_alphas_cumprod, noise, device=device
     )
-    eps = (
-        x_t - get_index_from_list(sqrt_alphas_cumprod, t, x_0.shape) * x_0
-    ) / get_index_from_list(sqrt_one_minus_alphas_cumprod, t, x_0.shape)
-    return F.mse_loss(eps, model(x_t, t), reduction=reduction)
+    return F.mse_loss(noise, model(x_t, t), reduction=reduction)

diffusion/ddpm/utils_torch/unet.py
@@ -330,6 +330,8 @@ class LitUnet(L.LightningModule):
     def __init__(
         self,
         unet: nn.Module,
+        betas: Tensor,
+        sqrt_recip_alphas: Tensor,
         sqrt_alphas_cumprod: Tensor,
         sqrt_one_minus_alphas_cumprod: Tensor,
         T: int,
@@ -341,6 +343,8 @@ class LitUnet(L.LightningModule):
     ):
         super().__init__()
         self.unet = unet
+        self.betas = betas
+        self.sqrt_recip_alphas = sqrt_recip_alphas
         self.sqrt_alphas_cumprod = sqrt_alphas_cumprod
         self.sqrt_one_minus_alphas_cumprod = sqrt_one_minus_alphas_cumprod
         self.T = T
@@ -394,7 +398,7 @@ class LitUnet(L.LightningModule):
                 (16, 1, self.img_size, self.img_size),
                 self.posterior_variance,
                 self.sqrt_one_minus_alphas_cumprod,
-                1.0 / torch.sqrt(1 - self.posterior_variance),
+                self.sqrt_recip_alphas,
                 self.T,
             )[-1]
 