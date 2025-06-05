0626bfd2044f6125b2374542c7091ecb0441c6ce
leonc <leonchat@orange.fr>
2025-06-05 05:46:13 +0100

    reformat sample functions

--- diffusion/ppca/run_pca_sampling.py
+++ diffusion/ppca/run_pca_sampling.py
@@ -7,36 +7,7 @@ from matplotlib import pyplot as plt
 from src.svd import naive_svd
 from src.svd import golub_kahan_svd
 from src.em import ppca
-
-
-def generate_sample_conditionned(X: np.array, keep: int, golub_svd: bool = True):
-    # can't have zero columns
-    X += 0.5 * np.random.randn(*X.shape)
-
-    if golub_svd:
-        U, S, V = golub_kahan_svd(X.copy(), max_step=2)
-    else:
-        U, S, V = naive_svd(X.copy())
-
-    s = np.diag(S).copy()
-    s[keep:] = 0.0
-
-    s = np.diag(s)
-
-    samples = U[:, :keep] @ s[:keep, :keep] @ V.T[:keep, :]
-
-    sample = samples[0]
-    sample = sample.reshape(28, 28)
-    return sample
-
-
-def get_samples_and_normalize(data: np.array, cl: np.array, n_samples: int, label: int):
-    X = [pil for (pil, l) in zip(data, cl) if l == label]
-    X = np.array([np.array(pil).reshape(784) for pil in X]) / 25.0
-
-    idx = random.choice(np.arange(len(X)), n_samples, replace=False)
-    X = X[idx]
-    return X
+from src.sample_utils import get_samples_and_normalize, generate_sample_conditionned
 
 
 if __name__ == "__main__":
