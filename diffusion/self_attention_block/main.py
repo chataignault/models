aedf5ddb3a6c084c83da3fd830a53b0c0b9400bc
chataignault <leonchat@orange.fr>
2025-10-13 17:14:13 +0100

    complete scaled dot attention block

--- diffusion/self_attention_block/main.py
+++ diffusion/self_attention_block/main.py
@@ -1,46 +1,48 @@
 from jax import numpy as jnp
 from jax import random
-from flax import linen as nn, compact
+from flax import linen as nn
+
 
 class ScaledDotProductAttentionBlock(nn.Module):
+    h_dim: int
+    out_dim: int
 
-    def __init__(self, h_dim:int, out_dim:int):
-        self.h_dim = h_dim
-        self.out_dim = out_dim
-
-    @compact
-    def __call__(self, x:jnp.array)->jnp.array:
+    @nn.compact
+    def __call__(self, x: jnp.array) -> jnp.array:
         q = nn.Dense(self.h_dim, name="dense_query")(x)
         k = nn.Dense(self.h_dim, name="dense_key")(x)
         v = nn.Dense(self.out_dim, name="dense_value")(x)
 
-        qk = jnp.einsum('NLH,NlH->NLl', q, k)
+        qk = jnp.einsum("NLH,NlH->NLl", q, k)
 
         s = softmax(qk / jnp.sqrt(qk.shape[-1]))
 
         return s @ v
 
 
-def softmax(x:jnp.array)->jnp.array:
-    return x
+def softmax(x: jnp.array) -> jnp.array:
+    return jnp.divide(jnp.exp(x), jnp.sum(jnp.exp(x), axis=-1)[:, :, jnp.newaxis])
 
 
 def main():
-
     print("Test attention block :")
 
     x = jnp.ones((100, 1000, 50))
-    rng = random.PRNGKey(0)
-    
+    rng = random.key(0)
+
     print(f"inpute shape : {x.shape}")
-    
+
     att = ScaledDotProductAttentionBlock(30, 30)
+    variables = att.init(rng, x)
 
     print(att)
 
-    y = att(x)
+    y = att.apply(variables, x)
+
+    print(y.shape)
 
     print("Forward pass successful.")
+
 
 if __name__ == "__main__":
     main()
